46ff826520260e6025127ba38986c23b57ba9a77
Angelo Fallaria <ba.fallaria@gmail.com>
2024-01-17 02:50:30 +0800

    refactor: extract middleware into own file

--- internal/api/handler.go
+++ internal/api/handler.go
@@ -37,22 +37,6 @@ func handleIndex() http.HandlerFunc {
 	}
 }
 
-func authRequired(f http.HandlerFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		trelloAPIKey := r.Header.Get("X-Trello-Api-Key")
-		trelloAPIToken := r.Header.Get("X-Trello-Api-Token")
-
-		if trelloAPIKey == "" || trelloAPIToken == "" {
-			component.RenderInfo(w, http.StatusUnauthorized, fmt.Errorf("To use this application, the Trello API key and token need to be supplied in the settings."), func(r htmx.Response) htmx.Response {
-				return r.AddTrigger(htmx.Trigger("open-settings"))
-			})
-			return
-		}
-
-		f(w, r)
-	}
-}
-
 func handleGetBoards() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		trelloAPIKey := r.Header.Get("X-Trello-Api-Key")
